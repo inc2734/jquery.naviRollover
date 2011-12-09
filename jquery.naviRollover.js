76557600cc23a0bd2295d8ad0ad7ead4b018bd44
inc2734 <inc@2inc.org>
2011-12-09 19:34:47 +0900

    ちょっと修正

--- jquery.naviRollover.js
+++ jquery.naviRollover.js
@@ -19,9 +19,9 @@
 * type : image
 *      リンク内の画像名が *_n.ext のものを、*_r.ext に置き換える
 */
-jQuery(function(){
-	jQuery.fn.naviRollOver = function(config){
-		navi = this;
+( function( $ ) {
+	$.fn.naviRollOver = function(config){
+		var navi = this;
 		var defaults = {
 			'root' : false ,		// ルートパス
 			'type' : 'html' ,		// タイプ(html or image)
@@ -30,56 +30,56 @@ jQuery(function(){
 			'tag' : 'ul:first li a',	// 処理をするhtmlタグを指定
 			'className' : 'cur'			// カレントリンクに付与されるclass名
 		};
-		topurl = '';	// トップページURLパス（実値）
-		var setting = jQuery.extend(defaults, config);
+		var topurl = '';	// トップページURLパス（実値）
+		var setting = $.extend(defaults, config);
 		var url;		// 現在のURLパス（ページ名は除く）
 		var naviArr;	// ナビゲーションのタグ（リンク）の集合
 		var navurl;		// ナビゲーションのURLパス
 		var baseUrl;	// トップページURLパス（基準値）
 
 		// 引数がちゃんとあるときだけ処理
-		if(setting.root){
+		if ( setting.root ) {
 			topurl = setting.root;
-			switch(setting.globalFlg){
+			switch ( setting.globalFlg ) {
 				case true :
 					// 現在のURLパスからトップ階層を省いたもの
-					url = getPathUnderSecondDir(location.pathname);
+					url = getPathUnderSecondDir( location.pathname );
 					break;
 				case false :
 					// 現在のURLパスを取得（その階層以下のページを省くため、一番後ろの / まで）。
-					url = getPathUntilLastSlash(location.pathname);
+					url = getPathUntilLastSlash( location.pathname );
 					break;
 			}
-			naviArr = navi.find(setting.tag);
-			jQuery.each(naviArr, function() {
-				_pathname = this.pathname;
+			var naviArr = navi.find( setting.tag );
+			$.each( naviArr, function() {
+				var _pathname = this.pathname;
 				// IE用
-				if(this.pathname.substring(0, 1) != '/'){
+				if ( this.pathname.substring( 0, 1 ) != '/' ) {
 					_pathname = '/' + this.pathname;
 				}
 
 				// 2階層目のディレクトリを基準に、現在のページがその下位であれば処理
 				// デフォルトは最後のディレクトリを基準に処理
-				switch(setting.globalFlg){
+				switch ( setting.globalFlg ) {
 					case true :
-						navurl = getPathUnderSecondDir(_pathname);
+						navurl = getPathUnderSecondDir( _pathname );
 						baseUrl = '/';
 						break;
 					case false :
-						navurl = getPathUntilLastSlash(_pathname);
+						navurl = getPathUntilLastSlash( _pathname );
 						baseUrl = topurl;
 						break;
 				}
 				// 一致したのが見つかったら
-				if(url.indexOf(navurl) == 0 && navurl != baseUrl || navurl == url){
-					switch(setting.type){
+				if ( url.indexOf( navurl ) == 0 && navurl != baseUrl || navurl == url ) {
+					switch ( setting.type ) {
 						case 'html' :
-							jQuery(this).addClass(setting.className);
+							$(this).addClass( setting.className );
 							break;
 						case 'image' :
-							var currentImg = jQuery(this).children('img').attr('src').split("_n", 2);
+							var currentImg = $(this).children('img').attr('src').split( "_n", 2 );
 							var newCurrentImgSrc = currentImg[0] + "_r" + currentImg[1];
-							var currentImg = jQuery(this).children('img').attr({ src: newCurrentImgSrc });
+							var currentImg = $(this).children('img').attr({ src: newCurrentImgSrc });
 							break;
 					}
 					return setting.keepFlg;
@@ -89,11 +89,11 @@ jQuery(function(){
 
 		/* 最後の / の位置を返す
 		--------------------------------------------------*/
-		function getPathUntilLastSlash(str){
-			var c = str.lastIndexOf("/");	// 一番後ろの/の位置を取得（その階層以下のページを省くため）
-			var path = str.substring(0, c + 1);	// 頭から一番後ろの/までの文字列を返す
+		function getPathUntilLastSlash( str ) {
+			var c = str.lastIndexOf( "/" );			// 一番後ろの/の位置を取得（その階層以下のページを省くため）
+			var path = str.substring( 0, c + 1 );	// 頭から一番後ろの/までの文字列を返す
 			// /が複数ある時用
-			if(path.match(/^\/{2,}(.*)/i)) {
+			if ( path.match( /^\/{2,}(.*)/i ) ) {
 				path = '/' + RegExp.$1;
 			}
 			return path;
@@ -101,12 +101,12 @@ jQuery(function(){
 
 		/* 引数からトップ階層を省いたものを返す
 		--------------------------------------------------*/
-		function getPathUnderSecondDir(str){
-			var path = getPathUntilLastSlash(str);
-			path = path.substring(topurl.length);
-			var n = path.indexOf("/");
-			path = '/' + path.substring(0, n + 1);
+		function getPathUnderSecondDir( str ) {
+			var path = getPathUntilLastSlash( str );
+			path = path.substring( topurl.length );
+			var n = path.indexOf( "/" );
+			path = '/' + path.substring( 0, n + 1 );
 			return path;
 		}
 	};
-});
+})( jQuery );
